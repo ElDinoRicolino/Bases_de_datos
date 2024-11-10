c99b09c7a02244308e52ebf05165670d82a867f9
RicardoRodriguez89 <al223229@alumnos.uacj.mx>
2024-11-09 22:08:06 -0700

    Cambios en el dise¤o y nuevos forms

---
+++ Base_Datos_II (2do Parcial)/Base_Datos_II/Base_Datos_II/Prestamos y devoluciones.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Base_Datos_II
+{
+    public partial class Prestamos_y_devoluciones : Form
+    {
+        public Prestamos_y_devoluciones()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelTitulo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Prestamos_y_devoluciones_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void materialButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void materialButton1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
